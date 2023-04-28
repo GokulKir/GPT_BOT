08804498f9c8154098e6aaa54c8d1cb9dfc2b2a4
GokulKir <gokulkrishna22072003@gmail.com>
2023-04-28 13:51:17 +0530

    Disign_Change

--- ROBO/AI.js
+++ ROBO/AI.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, useWindowDimensions, NativeEventEmitter, LogBox, ImageBackground, TextInput ,  } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, useWindowDimensions, NativeEventEmitter, LogBox, ImageBackground, TextInput, } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Voice from '@react-native-community/voice';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -23,9 +23,12 @@ import {
   responsiveWidth,
   responsiveFontSize
 } from "react-native-responsive-dimensions";
-import {TextAnimationFadeIn, TextAnimationZoom, TextAnimationRain, TextAnimationSlideDown, TextAnimationSlideUp, TextAnimationSlideLeft, TextAnimationSlideRight, TextAnimationShake, TextAnimationReverse, TextAnimationDeZoom} from 'react-native-text-effects';
+import { TextAnimationFadeIn, TextAnimationZoom, TextAnimationRain, TextAnimationSlideDown, TextAnimationSlideUp, TextAnimationSlideLeft, TextAnimationSlideRight, TextAnimationShake, TextAnimationReverse, TextAnimationDeZoom } from 'react-native-text-effects';
+import { useNavigation } from '@react-navigation/native';
 
-export default function AI() {
+export default function AI(props) {
+
+  const navigation = useNavigation()
 
   LogBox.ignoreLogs(['new NativeEventEmitter']);  // Ignore log notification by message
   LogBox.ignoreAllLogs();  // Ignore all log notifications
@@ -44,20 +47,21 @@ export default function AI() {
   const [subTrigger, setSubtrigger] = useState(false)
   const [inputMessage, setInputMessage] = useState("");
   const [outputMessage, setOutputMessage] = useState("The output message");
-  const [IsOrNot , setIsOrNot] = useState(false)
-  const [StartT , setStartT] = useState(false)
-  const [WelcomSp , setWelcomSp] = useState('')
+  const [IsOrNot, setIsOrNot] = useState(false)
+  const [StartT, setStartT] = useState(false)
+  const [WelcomSp, setWelcomSp] = useState('')
+  const [secondS , setSecondS] = useState(false)
 
-  const setRecodingResult = (data)=>{
-    
+  const setRecodingResult = (data) => {
+
     setResult(data)
   }
-  
+
 
   Voice.onSpeechEnd = () => setIsRecording(false)
   Voice.onSpeechError = err => setError(err.error)
   Voice.onSpeechResults = (result) => setRecodingResult(result.value[0])
-  
+
 
   // const handleTextInputMessage = (text) => {
   //   console.log(text);
@@ -65,22 +69,22 @@ export default function AI() {
   // };
 
 
-// useEffect(()=>{
-//   setInterval(() => {
-//     setStartT(true)
-    
-//   }, 2500);
-// })
+  // useEffect(()=>{
+  //   setInterval(() => {
+  //     setStartT(true)
+
+  //   }, 2500);
+  // })
 
 
-// Start recording //
+  // Start recording //
 
   const startRecording = async () => {
     // Voice.onSpeechRecognized((res)=>console.log(res))
 
     await Voice.start('en-US');
 
-   
+
 
   };
 
@@ -91,11 +95,11 @@ export default function AI() {
 
 
   const stopRecording = async () => {
-// console.log("stpo recoding*****");
+    // console.log("stpo recoding*****");
     setStarted(false)
-    setLoading(false) 
+    setLoading(false)
     setIsOrNot(true)
-     await Voice.stop()
+    await Voice.stop()
 
   }
 
@@ -103,95 +107,101 @@ export default function AI() {
     Tts.stop();
   }
 
-    //Stop recording //
+  //Stop recording //
 
-  
+
 
   // jestin xavier
   useEffect(() => {
- if(!IsTriger){
-   startRecording()
- }
- else{
-  stopRecording()
- }
-  }, [startRecording,IsTriger])
+    if (!IsTriger) {
+      startRecording()
+    }
+    else {
+      stopRecording()
+    }
+  }, [startRecording, IsTriger])
 
 
 
 
-  const TextToSpeech = (data)=>{
+  const TextToSpeech = (data) => {
     // console.log(IsTriger,'text to speech');
-// if(IsTriger){
+    // if(IsTriger){
 
-Tts.addEventListener('tts-finish', () => {
-  triggerGenerate(false)
-  console.log('Speech finished')
-} );
- Tts.speak(data, {
-    androidParams: {
-      KEY_PARAM_PAN: -1,
-      KEY_PARAM_VOLUME: 0.3,
-      KEY_PARAM_STREAM: 'STREAM_MUSIC',
-    },
-   
+    Tts.addEventListener('tts-finish', () => {
+      triggerGenerate(false)
+      console.log('Speech finished')
+    });
+    Tts.speak(data, {
+      androidParams: {
+        KEY_PARAM_PAN: -1,
+        KEY_PARAM_VOLUME: 0.3,
+        KEY_PARAM_STREAM: 'STREAM_MUSIC',
+      },
+
+    }
+
+
+    )
+    //  }
   }
-  
 
- )
-//  }
+
+
+  const triggerGenerate = (data) => {
+    setIsTriger(data)
   }
-
-
-
-const triggerGenerate =(data)=>{
-  setIsTriger(data)
-}
-const initialSetResult = ()=>{
-  setResult("")
-}
-const assistanceTrigger = (data) =>{
-  setAssistanc(data)
-}
+  const initialSetResult = () => {
+    setResult("")
+  }
+  const assistanceTrigger = (data) => {
+    setAssistanc(data)
+  }
 
 
   useEffect(() => {
 
     console.log(result, result === "Alexa" || result === "hi Alexa" || result === "hey Alexa", "******hey dana*****");
     // if (result === "Dana" || result === "hi Dana" || result === "hey Dana" || result == "hi Dyna" || result === "hi Diana"  ) {
-   
-    if ((result === "Daliya" || result === "hi Daliya" || result === "hey Daliya") && !assestant) {
+
+    if ((result === "Alexa" || result === "hi Alexa" || result === "hey Alexa") && !assestant) {
       triggerGenerate(true)
       initialSetResult()
       TextToSpeech("HI i am Daliya ")
       assistanceTrigger(true)
-    } 
-    
+    }
+
 
   }, [result])
 
 
 
-const VoiceController =  (data)=>{
-  setIsTriger(data)
-  
-}
+  const VoiceController = (data) => {
+    setIsTriger(data)
+
+  }
+
+  const Responcenavigate = (data) =>{
+     
+    setSecondS(data)
+
+  }
 
 
 
   useEffect(() => {
     console.log('====================================');
-    console.log(result );
+    console.log(result);
     console.log('====================================');
-    if (result.length > 0 ) {
+    if (result.length > 0) {
       // VoiceController(true)
-      if(!IsTriger && assestant ){
+      if (!IsTriger && assestant) {
         fetch("https://api.openai.com/v1/chat/completions", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
             Authorization:
-              "Bearer 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",
+              "Bearer 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",
           },
           body: JSON.stringify({
             // "prompt": inputMessage,
@@ -204,10 +214,17 @@ const VoiceController =  (data)=>{
           .then((data) => {
             console.log(data);
             console.log(data?.choices[0].message?.content);
+            
+           navigation.navigate("AnswerAI" , {data : data?.choices[0].message?.content})
+
+           
+
             setIsSpeak(data?.choices[0].message?.content)
             TextToSpeech(data?.choices[0].message?.content)
-            
-            
+
+          
+
+
             triggerGenerate(true)
             // Tts.speak(data?.choices[0].message?.content, {
             //   androidParams: {
@@ -216,16 +233,16 @@ const VoiceController =  (data)=>{
             //     KEY_PARAM_STREAM: 'STREAM_MUSIC',
             //   },
             // });
-    
-          
-    
-          })  
-          // setTimeout(function (){
-          //   VoiceController(false)
-          // }
-          //   , 5000);
-          
-        }
+
+
+
+          })
+        // setTimeout(function (){
+        //   VoiceController(false)
+        // }
+        //   , 5000);
+
+      }
     }
   }, [result])
 
@@ -261,19 +278,19 @@ const VoiceController =  (data)=>{
   //       //   },
   //       // });
 
-      
+
 
   //     })  
   //     // setTimeout(function (){
   //     //   VoiceController(false)
   //     // }
   //     //   , 5000);
-      
+
   //   }
 
   // }, [IsTriger])
-  
-  
+
+
 
 
 
@@ -325,68 +342,73 @@ const VoiceController =  (data)=>{
 
 
 
-   const Dimention = useWindowDimensions() ;
+  const Dimention = useWindowDimensions();
 
 
-   useEffect(() => {
-   
-   })
+
+
+  useEffect(() => {
+
+  })
 
 
   return (
     <View style={styles.container}>
 
-
-   
-   
-
-  <View style={{flex : 1 , backgroundColor:'#000' , alignItems:'center' , justifyContent:'space-between' }}>
-
-    
-
-  <View style={{top:40 , alignItems:'center'}}>
-
-    <ImageBackground style={{height:responsiveWidth(30) , width:responsiveWidth(30)  }} imageStyle={{borderRadius:200}} source={{uri : "https://i.pinimg.com/originals/fd/9f/6d/fd9f6dfa7872b4fa35a44d218cc77823.gif"}}>
-
-    </ImageBackground>
+    {secondS == false  ?  
 
 
 
-    <View style={{alignItems:'center' ,}}>
-
-      <View style={{ width:244 , height:244 , backgroundColor:'#000'  , alignItems:'center'}}>
-
-      <Text style={{color:'#fff' , fontSize:25 , fontWeight:'200'}}>{result}</Text>
-
-      </View>
+      <View style={{ flex: 1, backgroundColor: '#000', alignItems: 'center', justifyContent: 'space-between' }}>
 
 
-      <View style={{alignItems:'center'}}>
-
-        {IsSpeak.length > 0 ?         <TextAnimationFadeIn style={{color:'#fff' , fontSize:responsiveFontSize(2.5), fontWeight:'300' }} value={IsSpeak} delay={100} duration={1000}  />
-
- :   <TextAnimationFadeIn style={{color:'#fff' , fontSize:responsiveFontSize(2.5), fontWeight:'300' }} value={"Listening..."} delay={100} duration={1000}  /> }
 
 
-</View>
+        <View style={{ top: 40, alignItems: 'center'  }}>
 
 
-    </View>
-   
+         
+            <ImageBackground style={{ height: responsiveWidth(30), width: responsiveWidth(30) }} imageStyle={{ borderRadius: 200  , flex : 1}} source={{ uri: "https://i.pinimg.com/originals/fd/9f/6d/fd9f6dfa7872b4fa35a44d218cc77823.gif" }}>
 
-    </View>
-    
-    
-   
+            </ImageBackground>
 
-     
+
+       
+          <View style={{ alignItems: 'center', flex:1 }}>
+
+            {/* <View style={{ width: 244, height: 244, backgroundColor: '#000', alignItems: 'center' }}>
+
+              <Text style={{ color: '#fff', fontSize: 25, fontWeight: '200' }}>{result}</Text>
+
+            </View> */}
+
+
+            <View style={{ alignItems: 'center' }}>
+
+              {IsSpeak.length > 0 ? <TextAnimationFadeIn style={{ color: '#fff', fontSize: responsiveFontSize(2.5), fontWeight: '300' }} value={IsSpeak} delay={100} duration={1000} />
+
+                : <TextAnimationFadeIn style={{ color: '#fff', fontSize: responsiveFontSize(2.5), fontWeight: '300' }} value={"Listening..."} delay={100} duration={1000} />}
+
+
+            </View>
+
+
+          </View>
+
+
+        </View>
+
+
+
+
+
         <TouchableOpacity onPress={() => startRecording()} style={styles.floatingButton}>
           <View style={styles.buttonContainer}>
             {result == false ?
 
 
 
-              <ImageBackground  style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }} imageStyle={{ borderRadius: 200, elevation: 4, }} source={{ uri: 'https://img.freepik.com/free-photo/gradient-blue-abstract-background-smooth-dark-blue-with-black-vignette-studio_1258-66994.jpg' }}>
+              <ImageBackground style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }} imageStyle={{ borderRadius: 200, elevation: 4, }} source={{ uri: 'https://img.freepik.com/free-photo/gradient-blue-abstract-background-smooth-dark-blue-with-black-vignette-studio_1258-66994.jpg' }}>
 
                 <Image style={{ width: 70, height: 70 }} source={require('../assets/Mic.png')} />
 
@@ -403,16 +425,18 @@ const VoiceController =  (data)=>{
 
               //   </ImageBackground>
               // </TouchableOpacity>
-              <ImageBackground  style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }} imageStyle={{ borderRadius: 200, elevation: 4, }} source={{ uri: 'https://img.freepik.com/free-photo/gradient-blue-abstract-background-smooth-dark-blue-with-black-vignette-studio_1258-66994.jpg' }}>
+              <ImageBackground style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }} imageStyle={{ borderRadius: 200, elevation: 4, }} source={{ uri: 'https://img.freepik.com/free-photo/gradient-blue-abstract-background-smooth-dark-blue-with-black-vignette-studio_1258-66994.jpg' }}>
 
-              <Image style={{ width: 30, height: 30 }} source={require('../assets/Mic.png')} />
+                <Image style={{ width:responsiveWidth(2), height: responsiveWidth(2) }} source={require('../assets/Mic.png')} />
 
-            </ImageBackground>
+              </ImageBackground>
             }
           </View>
         </TouchableOpacity>
 
-        </View>
+      </View>
+
+        : <AnswerAI/>  }
 
 
 
@@ -442,7 +466,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 2,
-    borderRadius:200
+    borderRadius: 200
   },
   buttonContainer: {
     width: '100%',
--- ROBO/AnswerAI.js
+++ ROBO/AnswerAI.js
@@ -1,17 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler';
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import {TextAnimationFadeIn, TextAnimationZoom, TextAnimationRain, TextAnimationSlideDown, TextAnimationSlideUp, TextAnimationSlideLeft, TextAnimationSlideRight, TextAnimationShake, TextAnimationReverse, TextAnimationDeZoom} from 'react-native-text-effects';
+import { useRoute } from '@react-navigation/native';
 
-export default function AnswerAI() {
+export default function AnswerAI({props }) {
+
+    const route = useRoute()
+
+    const Dimention = useWindowDimensions() ;
+
+    const {data} = route.params
+
+    console.log(data.length);
+ 
+ 
+    const Ipsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, , letterSpacing: 3, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
   return (
     <View style={styles.container}>
-      <Text style={{color:'#fff'}}>AnswerAI</Text>
-    </View>
+
+        <ScrollView>
+            <View style={{width:Dimention.width , height : Dimention.height / 2 , backgroundColor:'#000' , alignItems:'center' ,}}>
+
+                <View style={{width:Dimention.width  - 40, height : Dimention.height /2.5 , backgroundColor:'#000' , top:20}}>
+
+
+                <Text  style={{color:'#fff' , fontSize:responsiveFontSize(1.6) , fontWeight:'300' , letterSpacing: 3,  }}>{data}</Text>
+
+                {/* <TextAnimationFadeIn  style={{color:'#fff' , fontSize:responsiveFontSize(1.6) , fontWeight:'300' , letterSpacing: 3,  }}  value={Ipsum} /> */}
+
+
+
+                </View>
+
+
+
+            </View>
+
+        </ScrollView>
+
+        <View style={{width: Dimention.width , height : Dimention.height /2 , backgroundColor:'#000'}}>
+
+
+            <View style={{alignItems:'center' , justifyContent:'center' , }}>
+                <Image style={{width:"100%" , height:"100%" , top:-10}} source={require('../assets/SoundL.gif')}/>
+            </View>
+
+        </View>
+   </View>
   )
 }
 
 const styles = StyleSheet.create({
     container : {
         flex : 1 ,
-        backgroundColor:'#000'
+        backgroundColor:'#000' , 
+        justifyContent:'space-evenly'
     }
 })
